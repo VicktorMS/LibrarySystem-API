3b20fba8eacefb0a4e62db4c6f13271f6a145b4c
Victor Moraes <victor.moraesrj@gmail.com>
2023-12-13 21:13:07 -0300

    Fixed pom.xml, using junit right version.

--- src/test/java/vicktorms/br/infnet/LibrarySysApiApplicationTests.java
+++ src/test/java/vicktorms/br/infnet/LibrarySysApiApplicationTests.java
@@ -2,8 +2,10 @@ package vicktorms.br.infnet;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
+@ContextConfiguration(classes = LibrarySysApiApplication.class)
 class LibrarySysApiApplicationTests {
 
 	@Test
